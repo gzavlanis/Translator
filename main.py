6d28a3e2074960326bbe03b4aabb3cd07f34de61
gzavlanis <gzavlanis@gmail.com>
2023-05-09 21:17:22 +0300

    extract files

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import psycopg2
+# import psycopg2
 from config import config
 from imports import importRegions, importCompetitions, importParticipants
 from exports import generator
 from model import regions_translator, competitions_translator, participants_translator
-
 
 """ Import CSV files """
 # regions = importRegions("data/regions.csv")
@@ -13,8 +12,8 @@ from model import regions_translator, competitions_translator, participants_tran
 # tennisCompetitions = importCompetitions("data/competitions_tennis.csv")
 
 # soccerParticipants = importParticipants("data/participants_soccer.csv")
-basketballParticipants = importParticipants("data/participants_basketball.csv")
-# tennisParticipants = importParticipants("data/participants_tennis.csv")
+# basketballParticipants = importParticipants("data/participants_basketball.csv")
+tennisParticipants = importParticipants("data/participants_tennis.csv")
 
 """ Languages to translate to """
 greek = "Greek"
@@ -36,11 +35,11 @@ persian = "Persian"
 # soccerParticipantsGR = participants_translator(soccerParticipants, greek)
 # soccerParticipantsPE = participants_translator(soccerParticipants, persian)
 
-basketballParticipantsGR = participants_translator(basketballParticipants, greek)
-basketballParticipantsPE = participants_translator(basketballParticipants, persian)
+# basketballParticipantsGR = participants_translator(basketballParticipants, greek)
+# basketballParticipantsPE = participants_translator(basketballParticipants, persian)
 
-# tennisParticipantsGR = engine(tennisParticipants, greek)
-# tennisParticipantsPE = engine(tennisParticipants, persian)
+tennisParticipantsGR = participants_translator(tennisParticipants, greek)
+tennisParticipantsPE = participants_translator(tennisParticipants, persian)
 
 """ New CSV files """
 # generator(regions, regionsGR, regionsPE, 'data/regions_translated.csv')
@@ -53,6 +52,6 @@ basketballParticipantsPE = participants_translator(basketballParticipants, persi
 
 # generator(soccerParticipants, soccerParticipantsGR, soccerParticipantsPE, 'data/participants_soccer_translated.csv')
 
-generator(basketballParticipants, basketballParticipantsGR, basketballParticipantsPE, 'data/participants_basketball_translated.csv')
+# generator(basketballParticipants, basketballParticipantsGR, basketballParticipantsPE, 'data/participants_basketball_translated.csv')
 
-# generator(soccerParticipants, soccerParticipantsGR, soccerParticipantsPE, 'data/participants_soccer_translated.csv')
+generator(tennisParticipants, tennisParticipantsGR, tennisParticipantsPE, 'data/participants_tennis_translated.csv')
